056ec7f96023e48c9b1223e76fc28c834c7613b0
prave <prave@LAPTOP-7V7RUD5N>
2019-06-22 16:57:10 +0530

    tough qstn fixed

--- src/main/java/main/ToughQuestion2.java
+++ src/main/java/main/ToughQuestion2.java
@@ -1,6 +1,4 @@
 package main;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,19 +13,23 @@ class ToughQuestion2 {
                         System.out.println("1.Bread only\n2.Bread+Jam\n3.Bread+Jam+Butter");
                         System.out.println("Enter the choice");
                         Integer choice = Integer.parseInt(br.readLine());
+                        Calories c = new Calories();
+                        
                         switch(choice)
                         {
                         case 1 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread
-                        	System.out.println();
+                        	
+                        	System.out.println(String.format("%.03f",c.calculateCalories(bread)*4.1868)+" kJ of energy generated from "+Math.round(bread*74)/1.0+" calories");
                         	break;
                         case 2 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	System.out.println("Enter the number teaspoon of Jam");
                         	jam = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread & jam
-                        	System.out.println();
+                        	System.out.println(String.format("%.03f",c.calculateCalories(bread,jam)*4.1868)+" kJ of energy generated from "+Math.round(((bread*74)+(jam*26)))/1.0+" calories");
+                        	
                         	break;
                         case 3 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
@@ -36,7 +38,8 @@ class ToughQuestion2 {
                         	System.out.println("Enter the number teaspoon of Butter");
                         	butter = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for  bread,jam & butter
-                        	System.out.println();
+                        	System.out.println(String.format("%.03f",c.calculateCalories(bread,jam,butter)*4.1868)+" kJ of energy generated from "+Math.round((bread*74)+(jam*26)+(butter*102))/1.0+" calories");
+                        	
                         	break;
                         }
                         //System.out.printf("%.3f",ca.calculateEnergy());
@@ -45,11 +48,18 @@ class ToughQuestion2 {
 }
 class Calories {
             
-            int calculateCalories() {
+            double calculateCalories(int slices) {
             //you have to overload this function for three parameters refer the question
-            	return -1;
+            	return  (Math.round(slices*74)/1.0) ;
             }
             
-            
-            
+            double calculateCalories(int slices, int jam) {
+                //you have to overload this function for three parameters refer the question
+                	return  (Math.round(((slices*74)+(jam*26)))/1.0) ;
+                }
+            double calculateCalories(int slices, int jam , int butter) {
+                //you have to overload this function for three parameters refer the question
+                	return  (Math.round((slices*74)+(jam*26)+(butter*102))/1.0) ;
+                }
+   
 }
